e9c2c2291a0b073a7b3ca4948db244251fe9a827
Vaughn English <venglishkc@gmail.com>
2026-07-09 14:53:29 -0700

    Add Collectible class for game items

---
+++ public/Collectible.mjs
@@ -0,0 +1,12 @@
+class Collectible {
+  constructor({ x, y, value, id }) {
+    this.x = x;
+    this.y = y;
+    this.value = value;
+    this.id = id;
+    this.width = 15;
+    this.height = 15;
+  }
+}
+
+export default Collectible;
